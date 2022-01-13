8c0b1e834da9457ff2ed4ffdcfd056ad9c20e0c7
daxaymakwana <daxaymakwana@gmail.com>
2022-01-13 05:53:04 +0530

    test: Test case for target by Id API

--- test/endpoints.js
+++ test/endpoints.js
@@ -77,10 +77,10 @@ test.serial('Create Target', async (t) => {
     // check target object
     const target = await toRedisPromise(redis.HGETALL)(redisNamespaces.targets.id(latestTargetSeq))
     t.is(Number(target.id), latestTargetSeq)
-    t.is(target.url, targetPayload.url)
-    t.is(target.value, targetPayload.value)
-    t.is(target.maxAcceptsPerDay, targetPayload.maxAcceptsPerDay)
-    t.is(target.accept, JSON.stringify(targetPayload.accept))
+    t.is(target.url, targetPayload.url, 'target url matched')
+    t.is(target.value, targetPayload.value, 'target value matched')
+    t.is(target.maxAcceptsPerDay, targetPayload.maxAcceptsPerDay, 'target maxAcceptsPerDay matched')
+    t.is(target.accept, JSON.stringify(targetPayload.accept), 'target accept matched')
   } catch (error) {
     t.fail('Error in Calling the API: ' + error)
   }
@@ -94,10 +94,27 @@ test.serial('List Targets', async (t) => {
     t.truthy(res.body.data, 'have data')
     t.truthy(res.body.data[0], 'have data[0]')
     t.truthy(res.body.data[0].id, 'have data[0].id')
-    t.is(res.body.data[0].url, mockTargets[0].url)
-    t.is(res.body.data[0].value, mockTargets[0].value)
-    t.is(res.body.data[0].maxAcceptsPerDay, mockTargets[0].maxAcceptsPerDay)
-    t.deepEqual(res.body.data[0].accept, mockTargets[0].accept)
+    t.is(res.body.data[0].url, mockTargets[0].url, 'target url matched')
+    t.is(res.body.data[0].value, mockTargets[0].value, 'target value matched')
+    t.is(res.body.data[0].maxAcceptsPerDay, mockTargets[0].maxAcceptsPerDay, 'target maxAcceptsPerDay matched')
+    t.deepEqual(res.body.data[0].accept, mockTargets[0].accept, 'target accept matched')
+  } catch (error) {
+    t.fail('Error in Calling the API: ' + error)
+  }
+})
+
+test.serial('Get Target By Id', async (t) => {
+  const currentTargetSeq = await toRedisPromise(redis.GET)(redisNamespaces.targets.seqCount) || 0
+  const url = `/api/targets/${currentTargetSeq}`
+  try {
+    const res = await servertestPromise(server(), url)
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.truthy(res.body.data, 'have data')
+    t.is(Number(res.body.data.id), currentTargetSeq, 'target id match')
+    t.is(res.body.data.url, mockTargets[0].url, 'target url matched')
+    t.is(res.body.data.value, mockTargets[0].value, 'target value matched')
+    t.is(res.body.data.maxAcceptsPerDay, mockTargets[0].maxAcceptsPerDay, 'target maxAcceptsPerDay matched')
+    t.deepEqual(res.body.data.accept, mockTargets[0].accept, 'target accept matched')
   } catch (error) {
     t.fail('Error in Calling the API: ' + error)
   }
